9ab314775f53b6d4c44b287f7117d06e43f59f78
sk84uhlivin <42231248+sk84uhlivin@users.noreply.github.com>
2018-11-09 18:24:47 -0500

    Leading zeroes and input display change.
    Added leading zeroes to 00 - 09 on 'list' and modified the instructions for user input.

--- shinrapassword.py
+++ shinrapassword.py
@@ -7,20 +7,20 @@ x = 0
 while programRunning:
 
     print()
-    second = input("What is the in-game second? Type 'quit' or 'list'. ")
+    second = input("Type the in-game second, 'list' or 'quit'. ")
 
     
 
     if second == 'list':
         while x < 60:
             if x%4 == 0:
-                print(x,'MAKO')
+                print(str(x).zfill(2),'MAKO')
             elif x%4 == 1:
-                print(x,'KING')
+                print(str(x).zfill(2),'KING')
             elif x%4 == 2:
-                print(x,'BEST')
+                print(str(x).zfill(2),'BEST')
             elif x%4 == 3:
-                print(x,'BOMB')
+                print(str(x).zfill(2),'BOMB')
             x+=1
 
     elif second == 'quit':

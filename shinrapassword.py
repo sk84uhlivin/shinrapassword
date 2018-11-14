72f4a9b321b82661466b550b36a9d6ff6ddf4ea6
sk84uhlivin <42231248+sk84uhlivin@users.noreply.github.com>
2018-11-14 07:58:30 +0000

    passList and Error Handling
    Added passList as a function to counter only being able to call the list once. Added Error Handling. I'm sure there's a better way to do it, but I couldn't figure it out. Therefore, I added the primary while loop to a try/except. I then had to add that try/except to another while loop so that the program would not close after 'except' triggers.

--- shinrapassword.py
+++ shinrapassword.py
@@ -2,39 +2,50 @@
 
 programRunning = True
 
-x = 0
+def passList():
+    x = 0
+    print()
+    while x < 60:
+        if x%4 == 0:
+            print(str(x).zfill(2),'MAKO')
+        elif x%4 == 1:
+            print(str(x).zfill(2),'KING')
+        elif x%4 == 2:
+            print(str(x).zfill(2),'BEST')
+        elif x%4 == 3:
+            print(str(x).zfill(2),'BOMB')
+        x+=1
 
 while programRunning:
+    try:
+        while programRunning:
 
-    print()
-    second = input("Type the in-game second, 'list' or 'quit'. ")
+            print()
+            second = input("Type the in-game second, 'list' or 'quit'. ").lower()
 
-    
+            if second == 'list':
+                passList()
 
-    if second == 'list':
-        while x < 60:
-            if x%4 == 0:
-                print(str(x).zfill(2),'MAKO')
-            elif x%4 == 1:
-                print(str(x).zfill(2),'KING')
-            elif x%4 == 2:
-                print(str(x).zfill(2),'BEST')
-            elif x%4 == 3:
-                print(str(x).zfill(2),'BOMB')
-            x+=1
+            elif second == 'quit':
+                programRunning = False
+                break 
 
-    elif second == 'quit':
-        programRunning = False
-        break 
+            elif int(second)%4 == 0:
+                print()
+                print('The password is MAKO (5).')
 
-    elif int(second)%4 == 0:
-        print('The password is MAKO (5).')
+            elif int(second)%4 == 1:
+                print()
+                print('The password is KING (2).')       
 
-    elif int(second)%4 == 1:
-        print('The password is KING (2).')       
+            elif int(second)%4 == 2:
+                print()
+                print('The password is BEST (1).')
+               
+            elif int(second)%4 == 3:
+                print()
+                print('The password is BOMB (4).')
 
-    elif int(second)%4 == 2:
-        print('The password is BEST (1).')
-       
-    elif int(second)%4 == 3:
-        print('The password is BOMB (4).')
+    except:
+        print()
+        print('Invalid command.')

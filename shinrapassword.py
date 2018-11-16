f585babbc34be384ebca112c53df36bbe5ef3e03
sk84uhlivin <sk84uhlivin@gmail.com>
2018-11-16 00:38:19 -0500

    Major changes. pass_list now prints in 3 columns, with the intention of being able to see the whole list at once. The modulo function now references a list instead of 4 if/elifs. Error handling was also vastly improved. The script now checks for verified commands, prints an error statement if command isn't found, and allows you to try again. This will most likely be the last version of the program that I contribute to. Twitter user "malpracticioner" helped tremendously with these changes. I wouldn't have been able to do some of this without their help (mainly pass_list print formatting).

--- shinrapassword.py
+++ shinrapassword.py
@@ -2,50 +2,44 @@
 
 programRunning = True
 
+passwords = ["MAKO (5)", "KING (2)", "BEST (1)", "BOMB (4)"]
+
+commands = ["list", "quit"]
+
 def passList():
-    x = 0
     print()
-    while x < 60:
-        if x%4 == 0:
-            print(str(x).zfill(2),'MAKO')
-        elif x%4 == 1:
-            print(str(x).zfill(2),'KING')
-        elif x%4 == 2:
-            print(str(x).zfill(2),'BEST')
-        elif x%4 == 3:
-            print(str(x).zfill(2),'BOMB')
-        x+=1
-
-while programRunning:
+    for x in range(20):
+        print(
+            "{:<02d} {} | {:<02d} {} | {:<02d} {}".format(
+                x,
+                passwords[x % 4],
+                x + 20,
+                passwords[(x + 20) % 4],
+                x + 40,
+                passwords[(x + 40) % 4],
+            )
+        )
+while programRunning:              
+    print()
+    cmd = input("Type the in-game second, 'list' or 'quit'. ").lower()
     try:
-        while programRunning:
+        cmd = int(cmd) % 4
 
+    except ValueError:
+        if cmd not in commands:
             print()
-            second = input("Type the in-game second, 'list' or 'quit'. ").lower()
+            print('Invalid command.')
+            continue
 
-            if second == 'list':
-                passList()
+    if cmd == 'list':
+        passList()
 
-            elif second == 'quit':
-                programRunning = False
-                break 
+    elif cmd == 'quit':
+        programRunning = False
+        break 
 
-            elif int(second)%4 == 0:
-                print()
-                print('The password is MAKO (5).')
-
-            elif int(second)%4 == 1:
-                print()
-                print('The password is KING (2).')       
-
-            elif int(second)%4 == 2:
-                print()
-                print('The password is BEST (1).')
-               
-            elif int(second)%4 == 3:
-                print()
-                print('The password is BOMB (4).')
-
-    except:
+    else:
         print()
-        print('Invalid command.')
+        print("The password is {}".format(passwords[cmd]) + ".")
+
+    
